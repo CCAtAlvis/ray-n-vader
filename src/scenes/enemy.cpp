703a2b3bd62c8df34e283f5f87d1c765eeb1945b
Mithil Poojary <mithil467@gmail.com>
2021-08-01 03:14:29 +0530

    Add enemy class

--- src/scenes/enemy.cpp
+++ src/scenes/enemy.cpp
@@ -1,4 +1,31 @@
 #include "raylib.h"
+#include <iostream>
+class Enemy
+{
+
+public:
+    float radius;
+    Vector2 center;
+
+    Enemy(float radius, int centerX, int centerY)
+    {
+        (*this).radius = radius;
+        (*this).center = {centerX, centerY};
+    }
+
+    void draw()
+    {
+        DrawCircle(center.x, center.y, radius, RED);
+    }
+
+    void update()
+    {
+        // get player's position
+        Vector2 player = {100, 100};
+        center.x += 1;
+        center.y += 1;
+    }
+};
 
 int main()
 {
@@ -11,18 +38,17 @@ int main()
 
     SetTargetFPS(60);
 
-    bool key = false;
+    Enemy e(10, 10, 15.0);
     while (!WindowShouldClose())
     {
-        // key = IsKeyDown(KEY_CAPS_LOCK);
-
+        ClearBackground(RAYWHITE);
         BeginDrawing();
 
-        // ClearBackground(RAYWHITE);
-
-        DrawCircle(10, 10, 15.0, RED);
+        e.draw();
 
         EndDrawing();
+
+        e.update();
     }
 
     CloseWindow();

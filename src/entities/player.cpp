2e23e317e12eb56e977b2396a0d8fe54b10631a2
Chinmay Chandak <chandakchinmay@gmail.com>
2021-08-01 09:32:30 +0530

    working on bullet

--- src/entities/player.cpp
+++ src/entities/player.cpp
@@ -6,6 +6,8 @@
 #include "raylib.h"
 #include "raymath.h"
 
+#include "../headers/bullet.h"
+
 // int screenWidth;
 // int screenHeight;
 int screenWidth = 800;
@@ -14,13 +16,14 @@ int screenMargin = 20;
 
 class Player
 {
-public:
+private:
     static const int SHIP_HEIGHT = 20;
     static const int SHIP_WIDTH = 20;
     static const int TURRET_SIZE = 20;
     static const int TURRET_TRANSLATION = 9;
     static constexpr float PLAYER_SPEED = 2.0;
 
+public:
     static Vector2 position;
     static Vector2 speed;
     static float acceleration;
@@ -30,7 +33,7 @@ public:
 
     static void Init()
     {
-        position = (Vector2){screenWidth / 2, screenHeight / 2 - SHIP_HEIGHT / 2};
+        position = {screenWidth / 2.0f, screenHeight / 2.0f - SHIP_HEIGHT / 2};
     }
 
     static void Draw()
@@ -95,8 +98,8 @@ public:
     }
 };
 
-Vector2 Player::position = (Vector2){0, 0};
-Vector2 Player::speed = (Vector2){100, 100};
+Vector2 Player::position = {0, 0};
+Vector2 Player::speed = {100, 100};
 float Player::acceleration = 0;
 float Player::rotation = 0;
 Color Player::playerColor = RED;
